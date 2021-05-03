d6ceac12fd95bd4fba112f65ce198afc4bdac7fc
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2021-05-03 15:22:20 +0200

    check args

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,4 +1,5 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include "../libft/includes/libftfull.h"
 
 #endif
--- sources/checker/main.c
+++ sources/checker/main.c
@@ -1,6 +1,38 @@
 #include "push_swap.h"
 
-int	main(void)
+int	check_one(char *str)
 {
+	int	i;
+
+	i = 0;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_args(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (check_one(argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (check_args(argc, argv))
+		ft_putstr("Error\n");
 	return (0);
 }
